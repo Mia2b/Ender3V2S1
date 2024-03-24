adf64ebe6d4e8d281c881814e1a1447f2f9f112f
Mia2b <mia2b@hotmail.com>
2024-03-23 20:07:54 -0700

    Ender 3 firmware for ezpz

--- configurations/ezpz-Ender3V2-422-BLTUBL-LA/Configuration.h
+++ configurations/ezpz-Ender3V2-422-BLTUBL-LA/Configuration.h
@@ -120,7 +120,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "ezpz-Ender3V2-422-BLTUBL-LA"
+#define CUSTOM_MACHINE_NAME "Ender3V2-422-BLTUBL-LA"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
